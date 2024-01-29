0525297ccbc32353eff5c7cc0f4e97b789dbe351
dmytroShev <dmytro.s@blaze.tech>
2024-01-29 11:16:10 +0200

    onfido test

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import {useEffect, useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [onfidoToken, setOnfidoToken] = useState<any>()
+
+
+  useEffect(() => {
+    fetch('https://us-central1-radiologexapp.cloudfunctions.net/getOnfidoSdkConfigPublic', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({"userEmail":"dmytro.s+52@blaize.tech"})
+    }).then(r => r.json()).then(r => setOnfidoToken(r))
+  }, []);
+
+  useEffect(() => {
+
+
+    (async () => {
+      const { init } = await import('onfido-sdk-ui');
+      if(!onfidoToken) return
+      init({
+        token: onfidoToken.token,
+        containerId: 'onfido-mount',
+        customUI: {
+          colorBackgroundSurfaceModal: '#17171e',
+          colorBorderSurfaceModal: '#23232C',
+          colorContentTitle: '#ffffff',
+          colorContentSubtitle: '#ffffff',
+          colorContentBody: '#707070',
+          colorContentButtonPrimaryText: '#ffffff',
+          colorBackgroundButtonPrimary: '#014FEF',
+          colorBackgroundSelector: '#17171e',
+          borderRadiusButton: '30px'
+        },
+        workflowRunId: onfidoToken.workflowRunId,
+        onComplete: () => {
+          console.log(111);
+        },
+        onError: () => {
+          console.log(1232)
+        }
+      });
+    })()
+  }, [onfidoToken]);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id={'onfido-mount'}></div>
     </>
   )
 }
